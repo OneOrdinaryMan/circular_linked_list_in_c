48a2e48182ff83dce3586cdbe459783440ef86a9
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-06-15 21:40:26 +0530

    commited the final main.

--- src/main.c
+++ src/main.c
@@ -24,7 +24,6 @@ int main() {
   }
   insert_at_index(&input_list, 2, 43);
   remove_at_index(&input_list, 2);
-  printf("%d\n", get_value(&input_list, 2));
   delete_list(&input_list);
   return 0;
 }
